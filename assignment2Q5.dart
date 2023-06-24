48fd944e5db36f21de5392c8087194fcf5aeb509
HuzaifJaamal <huzaifajamal95@gmail.com>
2023-06-24 16:46:55 +0500

    Assignment-2 All Question

--- assignment2Q5.dart
+++ assignment2Q5.dart
@@ -1,6 +1,7 @@
-// Q.5: Create a list of numbers & write a program to get the smallest & greatest number from a list.
+// Q.5: Create a map with name, phone keys and store some values to it.
+// Use where to find all keys that have length 4
 
 void main() {
-  List<int> numBers = [1, 5, 6, 8, 9, 13, 25, 22];
-  List<String> numBerStatus = [];
+  var studentRecord = {"name": "Huzaifa", "Phone": 03462564195};
+  print(studentRecord);
 }
