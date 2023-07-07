daa25bd622d010802818116559900d51c1683641
HuzaifJaamal <huzaifajamal95@gmail.com>
2023-07-07 15:17:05 +0500

    Assignment-2

--- assignment2Q5.dart
+++ assignment2Q5.dart
@@ -2,6 +2,18 @@
 // Use where to find all keys that have length 4
 
 void main() {
-  var studentRecord = {"name": "Huzaifa", "Phone": 03462564195};
-  print(studentRecord);
+  // Create a map with name and phone keys
+  Map<String, dynamic> laBle = {
+    "name": "Huzaifa",
+    "phone": 123456,
+    "city": "Karachi",
+    "address": "Karachi, Pakistan"
+  };
+
+  // Find keys length 4 using where method
+  List<String> keysLength4 =
+      laBle.keys.where((key) => key.length == 4).toList();
+
+  // Print the keys with length 4
+  print("Keys with length 4: $keysLength4");
 }
--- assignment2Q6.dart
+++ assignment2Q6.dart
@@ -1,4 +1,48 @@
-// Q.6: Create Map variable name world then inside it create countries Map, 
-// Key will be the name country & country value will have another map having 
-// capitalCity, currency and language to it. by using any country key print 
+// Q.6: Create Map variable name world then inside it create countries Map,
+// Key will be the name country & country value will have another map having
+// capitalCity, currency and language to it. by using any country key print
 // all the value of Capital & Currency.
+
+// import 'dart:io';
+
+void main() {
+  // Create the world map
+  Map<String, Map<String, String>> world = {
+    "USA": {
+      "capitalCity": "Washington D.C.",
+      "currency": "US Dollar",
+      "language": "English"
+    },
+    "Germany": {
+      "capitalCity": "Berlin",
+      "currency": "Euro",
+      "language": "German"
+    },
+    "Pakistan": {
+      "capitalCity": "Islambad",
+      "currency": "Rupees",
+      "language": "Urdu"
+    },
+    "Japan": {
+      "capitalCity": "Tokyo",
+      "currency": "Japanese Yen",
+      "language": "Japanese"
+    }
+  };
+  // Print the capital and currency of a specific country
+  String countryKey = "Pakistan";
+  print(world[countryKey]);
+  // exit;
+  var countryInfo = world[countryKey];
+
+  if (countryInfo != null) {
+    var capitalCity = countryInfo["capitalCity"];
+    var currency = countryInfo["currency"];
+
+    print("Country: $countryKey");
+    print("Capital City: $capitalCity");
+    print("Currency: $currency");
+  } else {
+    print("Country not found!");
+  }
+}
