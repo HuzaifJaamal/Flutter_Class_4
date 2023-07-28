2738ee20ca24d9a3a85a8ae2936edcfd162dc723
HuzaifJaamal <huzaifajamal95@gmail.com>
2023-07-29 01:19:34 +0500

    Assignment-2

--- assignment2Q14.dart
+++ assignment2Q14.dart
@@ -1,3 +1,15 @@
-// Q.14: Write a Dart code that takes in a list of integers and prints a new 
-// list with the elements sorted in ascending order. The original list should 
+// Q.14: Write a Dart code that takes in a list of integers and prints a new
+// list with the elements sorted in ascending order. The original list should
 // remain unchanged.
+
+void main() {
+  // Create a list of integers
+  List<int> numbers = [5, 7, 4, 9, 8, 1, 6];
+
+  // Sorted list
+  // numbers.sort();
+
+  // Print in ascending order
+  print(numbers.toList());
+  print(numbers);
+}
